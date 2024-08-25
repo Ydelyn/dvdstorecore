9f743cd898c45348ff2cb01d004128949c7fdc31
Lorris <colini.lorris@orange.fr>
2024-08-25 17:18:16 +0200

    32nd exercice

--- dvdstorecore/src/main/java/com/mycompany/dvdstore/core/entity/Movie.java
+++ dvdstorecore/src/main/java/com/mycompany/dvdstore/core/entity/Movie.java
@@ -1,10 +1,16 @@
 package com.mycompany.dvdstore.core.entity;
 
-import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Movie {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String genre;
